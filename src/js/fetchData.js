3049c6f5ec6fc0ebd6313816f14907c16534199c
Max Denishchuk <maks.denishchuk@gmail.com>
2022-09-10 00:18:50 +0300

    86778

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '22ffd9c27f352ff768d0db984597ea70';
 
+export async function trendFilms(page = 1) {
+  const axiosParams = { params: { api_key: API_KEY, page } };
+  return await axios.get(`${BASE_URL}/trending/all/day`, axiosParams);
+}
+
+export async function filmGenre(ids) {
+  const axiosParams = { params: { api_key: API_KEY, page } };
+}
