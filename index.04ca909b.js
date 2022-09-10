4b3b8f8331104e4fafed1087c1a2d0d72ccdfc7d
mamamamamaa <97800661+mamamamamaa@users.noreply.github.com>
2022-09-10 18:40:28 +0000

    Deploying to gh-pages from @ mamamamamaa/filmoteka-team-project@53201d30acaea4cc11d08e5ecc6e50861ab3cbfd 🚀

--- index.04ca909b.js
+++ index.04ca909b.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[futer-modal-open]"),closeModalBtn:document.querySelector("[futer-modal-close]"),modal:document.querySelector("[futer-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=my-library.88a8bdda.js.map
+//# sourceMappingURL=index.04ca909b.js.map
