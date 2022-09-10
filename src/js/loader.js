69f0e6d3ee038d13b526eb7a60bc956354354fe8
ProskurinV <proskyrinv.g@gmail.com>
2022-09-10 17:44:28 +0300

    scroll+bags

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,9 +1,17 @@
-import { Loading } from "notiflix";
+import { Loading } from 'notiflix';
+import OnlyScroll from 'only-scrollbar';
+
+const scroll = new OnlyScroll(window, {
+  damping: 0.4,
+});
 
 export const loaderOn = () => {
-    Loading.circle('Loading...', { backgroundColor: 'rgba(0,0,0,0.5)', svgColor: '#c7061a', });
-}
+  Loading.circle('Loading...', {
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    svgColor: '#c7061a',
+  });
+};
 
 export const loaderOff = () => {
-    Loading.remove(500);
-}   
+  Loading.remove(500);
+};
