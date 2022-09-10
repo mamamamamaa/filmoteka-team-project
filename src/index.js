5b8f424bec2236749c8c5433493e8c3236399f3b
Vadymshairan <95377527+Vadymshairan@users.noreply.github.com>
2022-09-10 20:25:35 +0300

    анимация на открытие модалки

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { trendFilms } from './js/fetchData';
 import card from './js/card-template';
-import toggleModal from './js/modal-film';
+import toggleModal from './js/modal-film-open-close';
 
 const refs = {
   cardBox: document.querySelector('.cards-container'),
