bd34f858b44dbcf238dfa38c33a62d47381366bc
Max Denishchuk <maks.denishchuk@gmail.com>
2022-09-10 19:08:48 +0300

    done genres

--- src/js/card-template.js
+++ src/js/card-template.js
@@ -1,22 +1,37 @@
 import { loaderOff } from './loader';
-import { filmGenre } from './fetchData';
 
-export default async function (data) {
+export default function (data, genres) {
   loaderOff();
   return data
     .map(el => {
-      const reliseDate = el.release_date
-        ? el.release_date.slice(0, 4)
-        : 'no data';
-      const genres = filmGenre(el.genre_ids);
-      console.log(genres);
+      const reliseDate = el.release_date ? el.release_date.slice(0, 4) : 'N/A';
+
+      let genre = [];
+      for (const id of el.genre_ids) {
+        genres.data.genres.forEach(e => {
+          if (e.id === id) {
+            genre.push(e.name);
+          }
+        });
+      }
+
+      let genreStr;
+      if (genre.length === 1) {
+        genreStr = `${genre[0]}`;
+      } else if (genre.length === 2) {
+        genreStr = `${genre[0]}, ${genre[1]}`;
+      } else if (genre.length > 2) {
+        genreStr = `${genre[0]}, ${genre[1]}, Other`;
+      }
       return `<div class="card-container">
                   <img src="https://image.tmdb.org/t/p/w500${
                     el.poster_path
-                  }" alt="${el.original_title}" class="film-img" />
+                  }" alt="${
+        el.original_title || el.original_name
+      }" class="film-img" />
                   <h2 class="film-title">${el.title || el.name}</h2>
                   <div class="info-container">
-                      <span class="film-genres">Actions, Bebra, Others |</span>
+                      <span class="film-genres">${genreStr} |</span>
                       <span class="film-relise">${reliseDate}</span>
                   </div>
               </div>`;
