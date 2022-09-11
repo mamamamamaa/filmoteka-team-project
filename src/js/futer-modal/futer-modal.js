a34302f23d7601493d7b4ab052a715b3ebb2d879
Nikolay Tankov <tankov.n.k@gmail.com>
2022-09-11 15:52:34 +0300

    no-skrolling test

--- src/js/futer-modal/futer-modal.js
+++ src/js/futer-modal/futer-modal.js
@@ -1,14 +1,29 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[futer-modal-open]'),
+//     closeModalBtn: document.querySelector('[futer-modal-close]'),
+//     modal: document.querySelector('[futer-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
+
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[futer-modal-open]'),
-    closeModalBtn: document.querySelector('[futer-modal-close]'),
-    modal: document.querySelector('[futer-modal]'),
+    openMenuBtn: document.querySelector('[futer-modal-open]'),
+    closeMenuBtn: document.querySelector('[futer-modal-close]'),
+    menu: document.querySelector('[futer-modal]'),
+    body: document.querySelector('.body'),
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
