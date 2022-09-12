0db03c74e4b32d7269d23b6a510ab47cd7f68c80
ProskurinV <97758596+ProskurinV@users.noreply.github.com>
2022-09-12 16:55:29 +0000

    Deploying to gh-pages from @ mamamamamaa/filmoteka-team-project@e8c2995877b52be88818d13bd13f9890ae5e900d 🚀

---
+++ my-library.5970c2f3.js
@@ -0,0 +1,2 @@
+const e={openMenuBtn:document.querySelector("[futer-modal-open]"),closeMenuBtn:document.querySelector("[futer-modal-close]"),menu:document.querySelector("[futer-modal]")};function n(){e.menu.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n);
+//# sourceMappingURL=my-library.5970c2f3.js.map
--- my-library.8ba37ba8.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=my-library.8ba37ba8.js.map
