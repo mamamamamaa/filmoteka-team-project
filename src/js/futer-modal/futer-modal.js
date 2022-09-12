ef490badbd305030c9e569d967c4e7d205d30943
Taras Kozmovych <taraskom0404@gmail.com>
2022-09-12 21:36:36 +0300

    close-modal

--- src/js/futer-modal/futer-modal.js
+++ src/js/futer-modal/futer-modal.js
@@ -1,14 +1,39 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[futer-modal-open]'),
-    closeModalBtn: document.querySelector('[futer-modal-close]'),
-    modal: document.querySelector('[futer-modal]'),
+    openMenuBtn: document.querySelector('[futer-modal-open]'),
+    closeMenuBtn: document.querySelector('[futer-modal-close]'),
+    menu: document.querySelector('[futer-modal]'),
   };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menu.addEventListener('click', bag);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  let isModal = false;
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+    isModal = !isModal;
+    if (isModal) {
+      window.addEventListener('keydown', onEscKeyPress);
+    } else {
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
+  }
+
+  function bag(event) {
+    if (event.currentTarget === event.target) {
+      refs.menu.classList.toggle('is-hidden');
+    }
+  }
+
+  function onEscKeyPress(event) {
+    console.log(event);
+    if (event.code === 'Escape') {
+      refs.menu.classList.add('is-hidden');
+      document.body.classList.remove('no-scroll');
+      isModal = false;
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
   }
 })();
--- src/js/modal-film-open-close.js
+++ src/js/modal-film-open-close.js
@@ -2,11 +2,44 @@ const refs = {
   cardBox: document.querySelector('.cards-container'),
   modal: document.querySelector('[data-modal]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
+  back: document.querySelector('[futer-modal]'),
+  backDropFilmCard: document.querySelector('[data-modal]'),
 };
 
 refs.cardBox.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.back.addEventListener('click', bacc);
+refs.backDropFilmCard.addEventListener('click', backDropFilm);
+
+let isModalShown = false;
 
 export default function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
+  isModalShown = !isModalShown;
+  if (isModalShown) {
+    window.addEventListener('keydown', onEscKeyPress);
+  } else {
+    window.removeEventListener('keydown', onEscKeyPress);
+  }
 }
+
+
+function bacc(event) {
+  if (event.currentTarget === event.target) {
+    refs.back.classList.add('is-hidden');
+  }
+}
+
+function backDropFilm(event) {
+  if (event.currentTarget === event.target) {
+    refs.backDropFilmCard.classList.toggle('is-hidden');
+  }
+}
+
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    toggleModal();
+  }
+}
+
+
