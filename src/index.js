48fc184d574ce87f1612f066cd8a1fad47908a5b
Max Denishchuk <maks.denishchuk@gmail.com>
2022-09-12 18:17:26 +0300

    ///

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { trendFilms, filmGenre, searchFilms } from './js/fetchData';
 import card from './js/card-template';
+import hugeCard from './js/hugeCard-template';
+import { searchGenres } from './js/searchGenres';
+
 import toggleModal from './js/modal-film-open-close';
 import { btnUpToTop, topFunction } from './js/btnUp';
- 
-import localStorageApi from './js/localStorageApi';
 
+import localStorageApi from './js/localStorageApi';
 
 const refs = {
   cardBox: document.querySelector('.cards-container'),
@@ -12,18 +14,16 @@ const refs = {
 
 // filmGenre().then(a => {
 //   trendFilms().then(e => {
-//     refs.cardBox.innerHTML += card(e.data.results, a);
+//     refs.cardBox.innerHTML += card(e.data.results, a, searchGenres);
+// btnUpToTop();
+// topFunction();
 //   });
 // });
 
 filmGenre().then(a => {
   searchFilms('Fury', 1).then(e => {
-    refs.cardBox.innerHTML += card(e.data.results, a);
-  })
-btnUpToTop();
-topFunction();
-
-
+    refs.cardBox.innerHTML += card(e.data.results, a, searchGenres);
+  });
+  btnUpToTop();
+  topFunction();
 });
-
-
