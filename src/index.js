239d5dc53343c68b50bce1f1f42c678d803a56bf
Max Denishchuk <maks.denishchuk@gmail.com>
2022-09-13 22:40:04 +0300

    //

--- src/index.js
+++ src/index.js
@@ -49,8 +49,6 @@ async function filmInfoFn(info) {
   refs.modaHugelCard.innerHTML = '';
   refs.modaHugelCard.insertAdjacentHTML('beforeend', hugeCard(info.data));
   writeLocalStorage(info.data);
-  btnUpToTop();
-  topFunction();
 }
 
 async function handleFormSubmit(e) {
